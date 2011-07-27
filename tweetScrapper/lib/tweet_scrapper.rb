3c3a9b9e9c20e184f8c3676415f323b6b46ad3b7
unknown <rbhochhibhoya@.telogical.com>
2011-07-27 08:35:15 -0500

    - Adding tweetScrapper ruby project - Completed scrapping the youtube links from tweets

---
+++ tweetScrapper/lib/tweet_scrapper.rb
@@ -0,0 +1,42 @@
+require 'net/http'
+require "json"
+class TweetScrapper
+   def displayTweets(searchWord)
+
+  end
+
+  def searchTweetsWithURIs(searchWord)
+
+  end
+  def searchTweetsWithRedirectedURIs(searchWord)
+
+  end
+
+  def displayYoutubeLinks(searchWord)
+    url = 'http://search.twitter.com/search.json?q='+searchWord
+    data = Net::HTTP.get_response(URI.parse(url)).body
+    result = JSON.parse(data)
+    printf "Total no. of Tweets found:#{result['results'].length}\n"
+    result['results'].each { |key|
+      if key['text'].include? "http:"
+        key['text'].split(" ").each { |possible_url|
+          if possible_url=~/http:/
+            resp = Net::HTTP.get_response(URI.parse(possible_url))
+              #Displaying URI  only if they are Redirected
+            if resp.code.eql?("301")
+              puts "Original Url: #{possible_url}"
+              redirectedUrl = resp["location"]
+              puts "Redirected Url: #{redirectedUrl}"
+                # Checking if Redirected URL is for youtube domain
+              puts "THIS IS YouTube" if URI.parse(redirectedUrl).host.include?("youtube")
+            end
+          end
+        }
+      end
+    }
+  end
+end
+
+tScapper = TweetScrapper.new
+puts "What word to scrap:"
+tScapper.displayYoutubeLinks(gets.strip)
