f4f8d345fc823593b5def5ab8f4a3bc3709ebac0
Rupesh <bhochhi@gmail.com>
2016-04-21 08:09:17 -0500

    work

--- simple-db/src/com/thumb/bhochhi/QueryStack.java
+++ simple-db/src/com/thumb/bhochhi/QueryStack.java
@@ -3,101 +3,127 @@ package com.thumb.bhochhi;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
+
 class Node {
 	private String command;
 	private String variable;
 	private String value;
-	
+
 	public void setCommand(String command) {
 		this.command = command;
 	}
-	public String getCommand(){
+
+	public String getCommand() {
 		return command;
 	}
-	
-	public void setVariable(String variable){
-		this.variable =  variable;
+
+	public void setVariable(String variable) {
+		this.variable = variable;
 	}
-	public String getVariable(){
+
+	public String getVariable() {
 		return variable;
 	}
-	public void setValue(String value){
+
+	public void setValue(String value) {
 		this.value = value;
 	}
-	public String getValue(){
-		return value;
-	}	
-}
-public class QueryStack{
 
-	private LinkedList<Node> store = new LinkedList<Node>();	
-	
-	public void setQuery(String command,String variable, String value){
-		Node node = new Node(){{
-			setCommand(command);
-			setVariable(variable);
-			setValue(value);
-		}};
-		store.addFirst(node);				
+	public String getValue() {
+		return value;
 	}
-	private boolean isBiginNode(Node node){
+
+	public String toString() {
+		return command + "  " + variable + " " + value;
+	}
+}
+
+public class QueryStack {
+
+	private LinkedList<Node> store = new LinkedList<Node>();
+
+	public void setQuery(String command, String variable, String value) {
+		Node node = new Node() {
+			{
+				setCommand(command);
+				setVariable(variable);
+				setValue(value);
+			}
+		};
+		store.addFirst(node);
+	}
+
+	private boolean isBiginNode(Node node) {
 		return node.getCommand().equals("BEGIN");
 	}
-	private boolean isUnSetNode(Node node){
+
+	private boolean isUnSetNode(Node node) {
 		return node.getCommand().equals("UNSET");
 	}
-	public String getQuery(String variable){
-		 Optional<Node> node = store.stream().filter(n-> !isBiginNode(n) && n.getVariable().equals(variable)).findFirst();
-		
-		 return node.isPresent() && !isUnSetNode(node.get())?node.get().getValue():null;
+
+	public String getQuery(String variable) {
+		Optional<Node> node = store.stream().filter(n -> !isBiginNode(n) && n.getVariable().equals(variable))
+				.findFirst();
+
+		return node.isPresent() && !isUnSetNode(node.get()) ? node.get().getValue() : null;
 	}
-	
-	public void unSetQuery(String command,String variable){
-		Node node = new Node(){{
-			setCommand(command);
-			setVariable(variable);
-		}};
-		store.addFirst(node);		
+
+	public void unSetQuery(String command, String variable) {
+		Node node = new Node() {
+			{
+				setCommand(command);
+				setVariable(variable);
+			}
+		};
+		store.addFirst(node);
 	}
 
 	public int numEqualTo(String value) {
 		ArrayList<String> unSetVariables = new ArrayList<String>();
 		ArrayList<String> matchedVariables = new ArrayList<String>();
 		Iterator<Node> iterater = store.iterator();
-		int count=0;
-		while(iterater.hasNext()){
+		while (iterater.hasNext()) {
 			Node node = iterater.next();
-			if(isUnSetNode(node) && !matchedVariables.contains(node.getVariable())){
-					unSetVariables.add(node.getVariable());
-			}
-			else if(!isUnSetNode(node) && !isBiginNode(node) && !matchedVariables.contains(node.getVariable()) && node.getValue().equals(value)){
+			if (isUnSetNode(node) && !matchedVariables.contains(node.getVariable())) {
+				unSetVariables.add(node.getVariable());
+			} else if (!isUnSetNode(node) && !isBiginNode(node) && !unSetVariables.contains(node.getVariable())
+					&& !matchedVariables.contains(node.getVariable()) && node.getValue().equals(value)) {
 				matchedVariables.add(node.getVariable());
 			}
-		}		
-		return matchedVariables.size();		
+		}
+		return matchedVariables.size();
 	}
 
 	public void beginTransaction(String command) {
-		Node node = new Node(){{
-			setCommand(command);
-		}};
-		store.addFirst(node);		
+		Node node = new Node() {
+			{
+				setCommand(command);
+			}
+		};
+		store.addFirst(node);
+	}
+
+	private Node getBeginNode() {
+		
+		return store.stream().filter(n -> n.getCommand().equals("BEGIN")).findFirst().orElse(null);
 	}
 
 	public String rollbackQuery(String command) {
-		Iterator<Node> iterater = store.iterator();
+		Node beginNode = getBeginNode();
+		int idx = store.indexOf(beginNode);
 		
-		while(iterater.hasNext()){
-			Node node = iterater.next();
-			if(node.getCommand().equals(command)){
-				System.out.println("begin node "+node);
-				
-				break;
+		if (beginNode == null) {
+			return "NO TRANSACTION";
+		} else {
+			while (true) {
+//				 Node node = store.peekFirst();
+//				 if(node)
+//				 if(node.getCommand().equals("BEGIN")){
+//				 break;
+//				 }
 			}
-		}		
-		System.out.println(iterater.next());
-		return "";
+		}
 	}
-		
 }
--- simple-db/test/com/thumb/bhochhi/CommandInterpreterTest.java
+++ simple-db/test/com/thumb/bhochhi/CommandInterpreterTest.java
@@ -5,16 +5,59 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CommandInterpreterTest {
-	QueryInterpreter ci;
-	private void given() {
+	private QueryInterpreter ci;
+	private String result;
+	@Test
+	public void testNumEqualToWithMultipleSet() {
 		ci = new QueryInterpreter();
 		ci.execute("Set a 10");
 		ci.execute("Set b 10");
+		result = ci.execute("numEqualto 10");
+		assertEquals("2", result);
 	}
+	
 	@Test
-	public void testNumEqualTo() {
-		given();
-		String result = ci.execute("numEqualto 10");
-		assertEquals("2", result);	
+	public void testNumEqualToWithMultipleSetOnSameVariableAndValue(){
+	
+		ci = new QueryInterpreter();
+		ci.execute("Set a 10");
+		ci.execute("Set b 10");
+		ci.execute("Set b 10");
+		ci.execute("Set a 10");
+		result = ci.execute("numEqualto 10");
+		assertEquals("2", result);
+		
+	}
+	
+	@Test
+	public void testNumEqualToWithMultipleSetOnSameVariableAndValueWithUnset(){
+		ci = new QueryInterpreter();
+		ci.execute("Set a 10");
+		ci.execute("Set b 10");
+		ci.execute("Set b 10");
+		ci.execute("unset b");
+		result = ci.execute("numEqualto 10");
+		assertEquals("1", result);
+	
+		ci = new QueryInterpreter();
+		ci.execute("Set a 10");
+		ci.execute("Set b 10");
+		ci.execute("Set b 10");
+		ci.execute("unset a");
+		result = ci.execute("numEqualto 10");
+		assertEquals("1", result);
+			
+	}
+	
+	@Test
+	public void testTransactionQuery(){
+		ci = new QueryInterpreter();
+		ci.execute("Set a 10");
+		ci.execute("Set b 10");
+		ci.execute("begin");
+		ci.execute("set a 5");
+		ci.execute("rollback");
+		result = ci.execute("get a");
+		assertEquals("10",result);
 	}
 }
